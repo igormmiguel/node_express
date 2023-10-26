8f5b512df350970ea1ebffd5944266fdf6a23c9c
Igor MM <Igor.mmiguel@eaportal.org>
2023-10-26 15:00:27 -0300

    recebendo parametros na rota

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express")
 const path = require("path")
+
 const app = express()
+
 const caminhoBase= path.join(__dirname, "templates")
+
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id =requisicao.params.id
+
+    console.log(`Acessando dados do usuario ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
 
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
